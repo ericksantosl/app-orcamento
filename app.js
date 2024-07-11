d23c3b6f755cd523e7cd5eae253271030144d816
ericksantosl <156433773+ericksantosl@users.noreply.github.com>
2024-07-11 19:24:23 -0300

    armazenamento local

--- app.js
+++ app.js
@@ -9,6 +9,32 @@ class Despesa {
     }
 }
 
+class Bd {
+    constructor() {
+        let id = localStorage.getItem('id')
+
+        if (id === null) {
+            localStorage.setItem('id', 0)
+        }
+    }
+
+
+    getProximoId() {
+        let proximoId = localStorage.getItem('id')
+        return parseInt(proximoId) + 1
+    }
+
+    gravar(d) {
+        let id = this.getProximoId()
+
+        localStorage.setItem(id, JSON.stringify(d))
+
+        localStorage.setItem('id', id)
+    }
+}
+
+let bd = new Bd()
+
 function cadastrarDespesa() {
     let ano = document.getElementById('ano')
     let mes = document.getElementById('mes')
@@ -19,5 +45,5 @@ function cadastrarDespesa() {
 
     let despesa = new Despesa(ano.value, mes.value, dia.value, tipo.value, descricao.value, valor.value)
 
-    console.log(despesa)
+    bd.gravar(despesa)
 }
