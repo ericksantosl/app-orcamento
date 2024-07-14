f9522d427b9a98462a6b530ddff974e46d946cd4
ericksantosl <156433773+ericksantosl@users.noreply.github.com>
2024-07-14 10:36:28 -0300

    verificando dados validos

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ class Despesa {
         this.descricao = descricao
         this.valor = valor
     }
+
+    validarDados() {
+        for (let i in this) {
+            if(this[i] == undefined || this[i] == '' || this[i] == null) {
+                return false
+            } else {
+                return true
+            }
+        }
+    }
 }
 
 class Bd {
@@ -45,5 +55,9 @@ function cadastrarDespesa() {
 
     let despesa = new Despesa(ano.value, mes.value, dia.value, tipo.value, descricao.value, valor.value)
 
-    bd.gravar(despesa)
+    if (despesa.validarDados()) {
+        bd.gravar(despesa)
+    } else {
+        console.log('Dados inválidos')
+    }
 }
